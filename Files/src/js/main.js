e40ef0f27f858d4005ff4a631eebb9bf69579845
underfr <loicklaurent28@gmail.com>
2025-06-18 19:48:24 +0200

    Update #4

--- Files/src/js/main.js
+++ Files/src/js/main.js
@@ -1,8 +1,19 @@
+/*------------------
+Tranparent Scrollbar
+------------------*/
+
 window.addEventListener('scroll', function(){
+    const link = document.querySelectorAll('.link');
     const head = this.document.querySelector('.head');
     if (window.scrollY > 50){
         head.classList.add('scrolled');
+        link.forEach(link => {
+            link.classList.add('linkBlack');
+        });
     } else {
         head.classList.remove('scrolled');
+        link.forEach(link => {
+            link.classList.remove('linkBlack');
+        });
     }
 });
